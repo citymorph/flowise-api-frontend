b7c16cb53cc8cfbd91bea07f9c29eb673214ab31
Paul Lu <paul.lu@citymorphstudio.com>
2023-09-18 17:53:18 -0700

    Added Simple Auth Control

--- index.js
+++ index.js
@@ -9,6 +9,37 @@ import { createPrediction } from "./controllers/flowise.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Authentication
+function authentication(req, res, next) {
+  const authheader = req.headers.authorization;
+  console.log(req.headers);
+
+  if (!authheader) {
+      let err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err)
+  }
+
+  const auth = new Buffer.from(authheader.split(' ')[1],
+      'base64').toString().split(':');
+  const user = auth[0];
+  const pass = auth[1];
+
+  if (user == process.env.FLOW_USER && pass == process.env.FLOW_PASS) {
+
+      // If Authorized user
+      next();
+  } else {
+      let err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+  }
+}
+
+// First step is the authentication of the client
+app.use(authentication)
 app.use(express.static("public"));
 
 // Use body-parser middleware to parse incoming request bodies
