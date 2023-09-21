56f5147c9279b22e742b24a0910381b1bb260c50
Paul Lu <paul.lu@citymorphstudio.com>
2023-09-21 12:26:58 -0700

    Setting up Kailee

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ const messageInput = document.getElementById("message");
 const responseEl = document.getElementById("response");
 const messageBtn = document.getElementById("message-btn");
 const talkVideo = document.getElementById('talk-video');
-//talkVideo.setAttribute('playsinline', '');
+talkVideo.setAttribute('playsinline', '');
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
@@ -36,8 +36,15 @@ form.addEventListener("submit", async (e) => {
 
 function playIdleVideo() {
   talkVideo.srcObject = undefined;
-  talkVideo.src = '/video/kailee_idle.mp4';
+  talkVideo.src = '/video/kailee_idle2.mp4';
   talkVideo.loop = true;
+}
+
+function playIntroVideo() {
+  talkVideo.srcObject = undefined;
+  talkVideo.src = '/video/kailee_intro.mp4';
+  talkVideo.loop = false;
+  responseEl.innerHTML = 'Hi I hope your day is well. My name is Kailee, and I am the AI assistant here at City Morph Studio. We are a product design lab based here in the South Bay. We specialize in web technologies and automation. What can I help you with today?';
 }
 
 playIdleVideo();
